6b07b6fe8d4cffc83bec2baeaeb5529d801fefb2
SarthakBagal <sarthakbagal04@gmail.com>
2026-02-10 11:21:06 +0530

    Updated authMiddleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,21 +1,38 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
-export const protect = async (req, res, next) => {
-  let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-    token = req.headers.authorization.split(" ")[1];
-  }
+// export const protect = async (req, res, next) => {
+//   let token;
+//   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+//     token = req.headers.authorization.split(" ")[1];
+//   }
 
-  if (!token) return res.status(401).json({ message: "Not authorized" });
+//   if (!token) return res.status(401).json({ message: "Not authorized" });
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = await User.findById(decoded.id).select("-password");
+//     next();
+//   } catch (error) {
+//     res.status(401).json({ message: "Token invalid or expired" });
+//   }
+// };
+
+export const protect = async (req, res, next) => {
+
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Not authorized, token missing" });
+  }
 
   try {
+    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select("-password");
+
+    req.user = decoded;
     next();
   } catch (error) {
-    res.status(401).json({ message: "Token invalid or expired" });
+    return res.status(401).json({ message: "Token invalid or expired" });
   }
 };
-
 
