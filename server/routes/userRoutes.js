33292ad07cb304073c6a4d4b36d09d4e1eb6870a
SarthakBagal <sarthakbagal04@gmail.com>
2026-02-11 12:39:34 +0530

    added dashboard in views and changes userRouters.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { getMe, updateMe } from "../controllers/userController.js";
+import path from "path";
 
 const router = express.Router();
 
 router.get("/profile", protect, getMe);
 router.put("/profile", protect, updateMe);
+
+router.get("/dashboard", protect, (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../Frontend/dashboard.html")
+  );
+});
 
 export default router;
 
